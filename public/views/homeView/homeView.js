2d6aefeb48c611225af676c87ef5e62295d56631
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-10-09 00:53:58 -0500

    the app asks for tokens first

--- public/views/homeView/homeView.js
+++ public/views/homeView/homeView.js
@@ -3,11 +3,14 @@
 angular.module('iotControl')
 .controller('HomeViewCtrl',
     [
-        '$state',
-        function($state) {
+        '$state', '$mdDialog', 'accessTokenSvc',
+        function($state, $mdDialog, accessTokenSvc) {
             "use strict";
 
             var vm = this;
+            if (accessTokenSvc.count() === 0) {
+                $state.go('add-token', { returnView: 'home' });
+            }
         }
     ]
 );
