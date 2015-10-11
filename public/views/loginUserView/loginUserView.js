0615e3190fd3ccdac79ec973ff23026df07ca7c2
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-10-11 18:29:07 -0500

    added a dummy view and dialog for login

---
+++ public/views/loginUserView/loginUserView.js
@@ -0,0 +1,21 @@
+/* global angular */
+
+angular.module('iotControl')
+.controller('LoginUserViewCtrl', 
+    [
+        '$state', 'server',
+        function($state, server) {
+            "use strict";
+
+            var vm = this;
+
+            vm.login = function() {
+
+            };
+
+            vm.signup = function() {
+                
+            };
+        }
+    ]
+);
