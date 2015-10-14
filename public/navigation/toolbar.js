9a7fdf0c36201e4b2e7f492061a158489254c48e
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-10-13 23:00:47 -0500

    added a login button to the toolbar

--- public/navigation/toolbar.js
+++ public/navigation/toolbar.js
@@ -3,19 +3,29 @@
 angular.module('iotControl')
 .controller('ToolbarCtrl', 
     [
-        '$mdSidenav', '$state',
-        function($mdSidenav, $state) {
+        '$mdSidenav', '$state', 'userSvc',
+        function($mdSidenav, $state, userSvc) {
             "use strict";
 
             var vm = this;
 
-            vm.isHome = function isHome() {
+            vm.isHome = function() {
                 return $state.is('home');
             };
 
-            vm.showNav = function showNav() {
+            vm.showNav = function() {
                 $mdSidenav('services').toggle();
             };
+
+            vm.login = function() {
+                userSvc.loginUser($state.current.name);
+            };
+
+            vm.showAccount = function() {
+                console.log('Navigating to the user account page');
+            }
+
+            vm.user = userSvc;
         }
     ]
 );
