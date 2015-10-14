bc2300222733f65e12ed287ba39ce914a3f8b02a
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-10-13 23:03:00 -0500

    login either displays view or dialog depending on media

--- public/services/userSvc/userSvc.js
+++ public/services/userSvc/userSvc.js
@@ -3,17 +3,72 @@
 angular.module('iotControl')
 .factory('userSvc',
     [
-        '$rootScope', '$state',
-        function($rootScope, $state) {
+        '$rootScope', '$q', '$state', '$mdDialog', '$mdMedia',
+        function($rootScope, $q, $state, $mdDialog, $mdMedia) {
             "use strict";
 
             var svc = this;
 
+            function LoginUserDialogCtrl($state, $mdDialog) {
+                var vm = this;
+
+                vm.hide = function() {
+                    $mdDialog.hide();
+                }
+
+                vm.login = function() {
+
+                };
+
+                vm.signup = function() {
+                    $state.go('createuser');
+                    $mdDialog.hide();
+                };
+            }
+
+            svc.isLoggedIn = function() {
+                return false;
+            };
+
+            svc.get = function(propName) {
+                return "Testerson";
+            };
+
             svc.loginUser = function(fromState) {
-                $state.go('login', { returnView: fromState });
+                var def = $q.defer();
+
+                if ($mdMedia('gt-md')) {
+                    $mdDialog.show({
+                        controller: LoginUserDialogCtrl,
+                        controllerAs: 'ctrl',
+                        templateUrl: 'views/loginUserView/loginUserDialog.tpl.html',
+                        parent: angular.element(document.body),
+                        locals: {
+                            fromState: fromState
+                        },
+                        bindToController: true
+                    })
+                    .then(
+                        function(result) {
+                            // parse the token object here and store it in $rootScope
+                            def.resolve();
+                        },
+                        function(error) {
+                            def.reject(error);
+                        }
+                    );
+                }
+                else {
+                    $state.go('login', { returnView: fromState });
+                    def.resolve();
+                }
+
+                return def.promise;
             };
 
             return {
+                isLoggedIn: svc.isLoggedIn,
+                get: svc.get,
                 loginUser: svc.loginUser
             };
         }
