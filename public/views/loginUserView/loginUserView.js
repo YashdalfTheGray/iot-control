15f40b0961548c2e8767740c8d9d940bccf77852
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-10-15 01:01:05 -0500

    preliminary hashing with SHA512 test

--- public/views/loginUserView/loginUserView.js
+++ public/views/loginUserView/loginUserView.js
@@ -10,7 +10,8 @@ angular.module('iotControl')
             var vm = this;
 
             vm.login = function() {
-
+                var SHA512 = new Hashes.SHA512();
+                console.log(SHA512.hex(vm.password));
             };
 
             vm.signup = function() {
