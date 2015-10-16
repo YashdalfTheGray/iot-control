6036ac015ca0e04ce62fcd9280f8fef32c8effba
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-10-16 02:45:56 -0500

    created a view and a controller for signing up

--- public/views/createUserView/createUserView.js
+++ public/views/createUserView/createUserView.js
@@ -3,11 +3,38 @@
 angular.module('iotControl')
 .controller('CreateUserViewCtrl',
     [
-        'server',
-        function(server) {
+        '$state', '$mdToast', 'server',
+        function($state, $mdToast, server) {
             "use strict";
 
             var vm = this;
+            var SHA512 = new Hashes.SHA512();
+            var ref = new Firebase(server.uri);
+
+            vm.signup = function() {
+                ref.createUser({
+                    email: vm.email,
+                    password: SHA512.hex(vm.password),
+                    firstName: vm.firstName,
+                    lastName: vm.lastName,
+                    tokens: vm.tokens
+                },
+                function(error, userData) {
+                    if (error) {
+                        console.log(error);
+                    }
+                    else {
+                        $mdToast.show(
+                            $mdToast.simple()
+                            .content('User successfully created. Please log in!')
+                            .position('bottom right')
+                            .hideDelay(3000)
+                        );
+                        console.log(userData);
+                        $state.go('home');
+                    }
+                });
+            };
         }
     ]
 );
