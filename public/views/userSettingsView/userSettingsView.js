06dcaa566c00fdc9ecd4a8d73e136ab00b5b3da9
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-10-18 14:41:42 -0500

    added a page for user settings

---
+++ public/views/userSettingsView/userSettingsView.js
@@ -0,0 +1,13 @@
+/* global angular */
+
+angular.module('iotControl')
+.controller('UserSettingsCtrl', 
+    [
+        'userSvc',
+        function(userSvc) {
+            "use strict";
+
+            var vm = this;    
+        }
+    ]
+);
