67589d05c651bdb0cf5dfc5303b76f306f00e4a0
YashdalfTheGray <yash.kulshrestha@gmail.com>
2015-10-19 20:03:47 -0500

    added more features around the login and logout

--- public/services/userSvc/userSvc.js
+++ public/services/userSvc/userSvc.js
@@ -3,15 +3,17 @@
 angular.module('iotControl')
 .factory('userSvc',
     [
-        '$rootScope', '$q', '$state', '$mdDialog', '$mdMedia',
-        function($rootScope, $q, $state, $mdDialog, $mdMedia) {
+        '$rootScope', '$q', '$state', '$mdDialog', '$mdMedia', '$mdToast', 'server', 'toast',
+        function($rootScope, $q, $state, $mdDialog, $mdMedia, $mdToast, server, toast) {
             "use strict";
 
             var svc = this;
+            var ref = new Firebase(server.uri);
 
-            function LoginUserDialogCtrl($state, $mdDialog, $mdToast, toast) {
+            $rootScope.account = {};
+
+            function LoginUserDialogCtrl($state, $mdDialog, $mdToast, userSvc) {
                 var vm = this;
-                var ref = new Firebase('https://iot-control.firebaseio.com');
 
                 vm.hide = function() {
                     $mdDialog.hide();
@@ -20,25 +22,16 @@ angular.module('iotControl')
                 vm.login = function() {
                     var SHA512 = new Hashes.SHA512();
                     
-                    ref.authWithPassword({
-                        email: vm.email,
-                        password: SHA512.hex(vm.password)
-                    },
-                    function(error, authData) {
-                        if (error) {
+                    userSvc.loginUser(vm.email, SHA512.hex(vm.password))
+                    .then(
+                        function(result) {
+                            console.log(result);
+                            $mdDialog.hide(result);
+                        },
+                        function(error) {
                             console.log(error);
                         }
-                        else {
-                            $mdToast.show(
-                                $mdToast.simple()
-                                .content('User login successful!')
-                                .position(toast.position)
-                                .hideDelay(toast.durationLong)
-                            );
-                            console.log(authData);
-                            $mdDialog.hide(authData);
-                        }
-                    }, { remember: 'sessionOnly' });
+                    );
                 };
 
                 vm.signup = function() {
@@ -48,11 +41,21 @@ angular.module('iotControl')
             }
 
             svc.isLoggedIn = function() {
-                return false;
+                if(!$rootScope.account) {
+                    return false;
+                }
+                else {
+                    return $rootScope.account.loggedIn;
+                }
             };
 
             svc.get = function(propName) {
-                return "Testerson";
+                if(propName === 'firstName' && !$rootScope.account.firstName) {
+                    return 'New User';
+                }
+                else {
+                    return $rootScope.account[propName];
+                }
             };
 
             svc.showLogin = function(fromState) {
@@ -88,10 +91,49 @@ angular.module('iotControl')
                 return def.promise;
             };
 
+            svc.loginUser = function(email, hashPassword) {
+                var def = $q.defer();
+
+                ref.authWithPassword(
+                    {
+                        email: email,
+                        password: hashPassword
+                    },
+                    function(error, authData) {
+                        if (error) {
+                            def.reject(error);
+                        }
+                        else {
+                            $mdToast.show(
+                                $mdToast.simple()
+                                .content('User login successful!')
+                                .position(toast.position)
+                                .hideDelay(toast.durationLong)
+                            );
+                            $rootScope.account.loggedIn = true;
+                            $rootScope.account.uid = authData.uid;
+                            $rootScope.account.token = authData.token;
+                            $rootScope.account.authData = authData;
+
+                            def.resolve($rootScope.account);
+                        }
+                    },
+                    { remember: 'sessionOnly' }
+                );
+
+                return def.promise;
+            }
+
+            svc.logoutUser = function() {
+                $rootScope.account = {};
+            };
+
             return {
                 isLoggedIn: svc.isLoggedIn,
                 get: svc.get,
-                showLogin: svc.showLogin
+                showLogin: svc.showLogin,
+                loginUser: svc.loginUser,
+                logoutUser: svc.logoutUser
             };
         }
     ]
--- public/views/loginUserView/loginUserView.js
+++ public/views/loginUserView/loginUserView.js
@@ -3,34 +3,25 @@
 angular.module('iotControl')
 .controller('LoginUserViewCtrl', 
     [
-        '$state', '$mdToast', 'server', 'toast',
-        function($state, $mdToast, server, toast) {
+        '$state', '$mdToast', 'userSvc',
+        function($state, $mdToast, userSvc) {
             "use strict";
 
             var vm = this;
-            var ref = new Firebase('https://iot-control.firebaseio.com');
 
             vm.login = function() {
                 var SHA512 = new Hashes.SHA512();
                 
-                ref.authWithPassword({
-                    email: vm.email,
-                    password: SHA512.hex(vm.password)
-                },
-                function(error, authData) {
-                    if (error) {
+                userSvc.loginUser(vm.email, SHA512.hex(vm.password))
+                .then(
+                    function(result) {
+                        console.log(result);
+                        $state.go('home');
+                    },
+                    function(error) {
                         console.log(error);
                     }
-                    else {
-                        $mdToast.show(
-                            $mdToast.simple()
-                            .content('User login successful!')
-                            .position(toast.position)
-                            .hideDelay(toast.durationLong)
-                        );
-                        console.log(authData);
-                    }
-                });
+                );
             };
 
             vm.signup = function() {
